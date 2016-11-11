b91abca584bb7d7977ca77aaebc53ccd0250e69a
Emily Fiennes <etw.fiennes@gmail.com>
2016-11-11 18:31:17 +0100

    Update application helper for default image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,10 @@ module ApplicationHelper
 
   def photo_path photo
     if photo
-      cl_image_path photo.path, height: 300, width: 400, crop: :fit, :effect=>"blur:100"
+      cl_image_path photo.path, height: 300, width: 400, crop: :thumb, :effect=>"blur:100"
     else
-      "http://www.magnificentbastard.com/images/pics/empty-cocktail-glass.jpg"
+      cl_image_path("http://res.cloudinary.com/dbiuujogd/image/upload/v1478883903/cocktails_tlhl0g.png", height: 150, width: 200, crop: :fit, :effect=>"blur:100")
     end
   end
 end
-
 
