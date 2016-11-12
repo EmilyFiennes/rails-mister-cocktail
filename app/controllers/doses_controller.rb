83ac2e8c9b5a8ebe392a5d6c7a81058781ec3594
Emily Fiennes <etw.fiennes@gmail.com>
2016-11-12 15:59:00 +0100

    Add alert to cocktail creation and dose deletion

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -13,7 +13,9 @@ class DosesController < ApplicationController
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = @cocktail.doses.new(dose_params)
+    @ingredient = @dose.ingredient.name
     if @dose.save
+      flash[:notice] = "New dose:  #{@dose.description} of #{@dose.ingredient.name} has been added"
       redirect_to cocktail_path(@cocktail) #render 'cocktails/show'
     else
       render 'cocktails/show'
@@ -23,6 +25,7 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy #deletes from db but doesn't delete the instance which still has the id of the cocktail
+    flash[:alert] = "Dose: #{@dose.description} of #{@dose.ingredient.name} deleted"
     redirect_to cocktail_path @dose.cocktail
   end
 
