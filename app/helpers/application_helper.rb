50d3ab2e68efdcd9903db49265bd8ab80865f8f7
Emily Fiennes <etw.fiennes@gmail.com>
2016-11-12 17:36:14 +0100

    Update index with footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,20 @@ module ApplicationHelper
       cl_image_path("http://res.cloudinary.com/dbiuujogd/image/upload/v1478883903/cocktails_tlhl0g.png", height: 150, width: 200, crop: :fit, :effect=>"blur:100")
     end
   end
+
+  def bootstrap_class_for(type)
+    case type
+    when :alert then "alert-error"
+    when :notice then "alert-success"
+    end
+  end
+
+  def yield_with_default(holder, default)
+    if content_for?(holder)
+      content_for(holder).squish
+    else
+      default
+    end
+  end
 end
 
